67630304719f317ff40ffb2fad3d2d874f33f76f
Coldstuff1 <samuel.faubert@mail.mcgill.ca>
2024-03-16 13:23:55 -0400

    merged ros on new code

--- stm_workspace/hydrophone/Core/Src/main.cpp
+++ stm_workspace/hydrophone/Core/Src/main.cpp
@@ -86,31 +86,24 @@ volatile uint16_t adcChannels[4];
 volatile int conversionComplete = 0;
 
 typedef enum {
+	INIT,
 	HYDROPHONE1,
 	HYDROPHONE2,
 	HYDROPHONE3
 } Hydrophone;
 
-struct Payload {
-	Hydrophone hydrophone;
-	uint32_t frequency;
-	uint32_t time;
-};
-
-typedef struct Payload Payload;
-
-__always_inline Payload* createPayload(uint32_t frequency, uint32_t time, Hydrophone hydrophone) {
-	Payload *payloadPtr = (Payload *) malloc(sizeof(Payload));
-	payloadPtr->frequency = frequency;
-	payloadPtr->time = time;
-	payloadPtr->hydrophone = hydrophone;
-	return payloadPtr;
-}
+volatile Hydrophone curPhone = INIT;
 
 __always_inline void calculateVoltage(uint16_t VREFINT_DATA, uint16_t ADC_DATA, float32_t *pOut) {
 	float32_t VREFINT_CAL = (float32_t) *((uint16_t*) VREFINT_CAL_ADDR);
 	float32_t Vdda = 3.0 * (VREFINT_CAL / VREFINT_DATA);
 	*pOut = (Vdda / 4095) * (float32_t)ADC_DATA;
+}
+
+__always_inline void calculateVariance(float32_t *pSum, float32_t *pSumSquares, float32_t *pResult) {
+	*pResult = ((*pSumSquares) - ((powf((*pSum), 2))/512.0f)) / (512.0f - 1.0f);
+	*pSum = 0;
+	*pSumSquares = 0;
 }
 
 void HAL_UART_TxCpltCallback(UART_HandleTypeDef *huart){
@@ -163,24 +156,34 @@ int main(void)
   MX_USART2_UART_Init();
   MX_ADC1_Init();
   /* USER CODE BEGIN 2 */
-  float32_t hydrophone0[1024];
   float32_t hydrophone1[1024];
   float32_t hydrophone2[1024];
-  float32_t V2, V3, V4;
-  uint32_t frequency;
+  float32_t hydrophone3[1024];
+  float32_t V1, V2, V3;
+  float32_t v1Variance;
+  float32_t v1Sum = 0;
+  float32_t v1SumSquares = 0;
   float32_t v2Variance;
   float32_t v2Sum = 0;
   float32_t v2SumSquares = 0;
+  float32_t v3Variance;
+  float32_t v3Sum = 0;
+  float32_t v3SumSquares = 0;
+  uint32_t index = 0;
   HAL_ADCEx_Calibration_Start(&hadc1, ADC_SINGLE_ENDED);
   for (int i = 0; i < 512; i++) {
-	  hydrophone0[2*i + 1] = 0;
 	  hydrophone1[2*i + 1] = 0;
 	  hydrophone2[2*i + 1] = 0;
+	  hydrophone3[2*i + 1] = 0;
   }
+  hmsg1.hydrophone = 1;
+  hmsg2.hydrophone = 2;
+  hmsg3.hydrophone = 3;
   nh.initNode();
   nh.advertise(hpub1);
   nh.advertise(hpub2);
   nh.advertise(hpub3);
+  HAL_TIM_Base_Start_IT(&htim2);
   /* USER CODE END 2 */
 
   /* Infinite loop */
@@ -189,40 +192,72 @@ int main(void)
   //printf("HI!\n\r");
   while (1)
   {
-	  HAL_Delay(400);
-//	  printf("%lu\r\n", usecs_elapsed);
-	  for(int i = 0; i < 512; i++) {
+    index = 0;
+	  for(int i = 0; i < 1536; i++) {
 		  HAL_ADC_Start_DMA(&hadc1, (uint32_t *) adcChannels, 4);
 		  while (conversionComplete == 0) {
 			  continue;
 		  }
 		  conversionComplete = 0;
-		  calculateVoltage(adcChannels[0], adcChannels[1], &V2);
-		  calculateVoltage(adcChannels[0], adcChannels[2], &V3);
-		  calculateVoltage(adcChannels[0], adcChannels[3], &V4);
-		  v2Sum += V2;
-		  v2SumSquares += powf(V2, 2);
-		  hydrophone0[2*i] = V2;
-		  hydrophone1[2*i] = V3;
-		  hydrophone2[2*i] = V4;
-	  }
-	  //printf("%f\r\n", v2Sum);
-	  //printf("%f\r\n", v2SumSquares);
-	  v2Variance = (v2SumSquares - ((powf(v2Sum, 2))/512.0f)) / (512.0f - 1.0f);
+      if (i == 0)
+        hmsg1.time = usecs_elapsed;
+      if (i == 1)
+        hmsg2.time = usecs_elapsed;
+      if (i == 3)
+        hmsg3.time = usecs_elapsed;
+      switch (curPhone) {
+      	case INIT:
+      		break;
+      	case HYDROPHONE1:
+      		calculateVoltage(adcChannels[0], adcChannels[1], &V1);
+      		v1Sum += V1;
+      		v1SumSquares += powf(V1, 2);
+      		hydrophone1[2*index] = V1;
+      		break;
+        case HYDROPHONE2:
+        	calculateVoltage(adcChannels[0], adcChannels[2], &V2);
+      		v2Sum += V2;
+      		v2SumSquares += powf(V2, 2);
+        	hydrophone2[2*index] = V2;
+        	break;
+        case HYDROPHONE3:
+        	calculateVoltage(adcChannels[0], adcChannels[3], &V3);
+        	hydrophone3[2*index] = V3;
+      		v3Sum += V3;
+      		v3SumSquares += powf(V3, 2);
+        	break;
+      }
+      if (i % 3 == 2) {
+    	  index++;
+      }
+	 }
+    calculateVariance(&v1Sum, &v1SumSquares, &v1Variance);
+    calculateVariance(&v2Sum, &v2SumSquares, &v2Variance);
+    calculateVariance(&v3Sum, &v3SumSquares, &v3Variance);
+    hmsg1.frequency = get_frequency(hydrophone1, 1024, 4705882.3529);
+    hmsg2.frequency = get_frequency(hydrophone2, 1024, 4705882.3529);
+    hmsg3.frequency = get_frequency(hydrophone3, 1024, 4705882.3529);
 	  v2Sum = 0;
 	  v2SumSquares = 0;
-	  frequency = get_frequency(hydrophone0, 1024, 4705882.3529);
-	  if (v2Variance > 0.9) {
-		  //printf("variance of hydrophone 1: %f\r\n", v2Variance);
-		  Payload *payload1 = createPayload(frequency, usecs_elapsed, HYDROPHONE1);
-		  updateMessage(hmsg1, frequency, usecs_elapsed, 1);
-		  //printf("frequency from hydrophone 1: %lu\r\n", payload1->frequency);
-		  //printf("time from hydrophone 1: %lu\r\n", payload1->time);
-	  }
-	  hpub1.publish(&hmsg1);
+    if (v1Variance > 0.001) {
+    	printf("variance of hydrophone 1: %f\r\n", v1Variance);
+		  printf("frequency from hydrophone 1: %lu\r\n", hmsg1.frequency);
+		  printf("time from hydrophone 1: %lu\r\n", hmsg1.time);
+      hpub1.publish(&hmsg1);
+    }
+    if (v2Variance > 0.001) {
+    	printf("variance of hydrophone 2: %f\r\n", v2Variance);
+		  printf("frequency from hydrophone 2: %lu\r\n", hmsg2.frequency);
+		  printf("time from hydrophone 2: %lu\r\n", hmsg2.time);
+      hpub2.publish(&hmsg2);
+    }
+    if (v3Variance > 0.001) {
+    	printf("variance of hydrophone 3: %f\r\n", v3Variance);
+		  printf("frequency from hydrophone 3: %lu\r\n", hmsg3.frequency);
+		  printf("time from hydrophone 3: %lu\r\n", hmsg3.time);
+      hpub3.publish(&hmsg3);
+    }
 	  nh.spinOnce();
-	  //printf("frequency from hydrophone 2: %lu", get_frequency(hydrophone1, 1024, 4705882.35));
-	  //printf("frequency from hydrophone 3: %lu", get_frequency(hydrophone2, 1024, 4705882.35));
 
     /* USER CODE END WHILE */
 
@@ -509,11 +544,22 @@ void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim) {
 	usecs_elapsed += 10;
 }
 
-/*void HAL_ADC_LevelOutOfWindowCallback(ADC_HandleTypeDef *hadc) {
-}*/
 
 void HAL_ADC_ConvCpltCallback(ADC_HandleTypeDef *hadc) {
-	conversionComplete = 1;
+	switch (curPhone) {
+	case INIT:
+		curPhone = HYDROPHONE1;
+		break;
+	case HYDROPHONE1:
+		curPhone = HYDROPHONE2;
+		break;
+	case HYDROPHONE2:
+		curPhone = HYDROPHONE3;
+		break;
+	case HYDROPHONE3:
+		curPhone = HYDROPHONE1;
+  }
+  conversionComplete = 1;
 }
 /* USER CODE END 4 */
 
