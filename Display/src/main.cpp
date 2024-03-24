d11929bbcf1dba9c4d9ff8be20c893cf2fab2579
BarryZe <98925337+BarryZe@users.noreply.github.com>
2024-03-24 15:27:18 -0400

    Update main.cpp
    Implemented ROSserial with one voltage topic, the code displays the voltage. This code is directly merged into the main and built in an Arduino IDE env. If it does not work pls code and paste this file into Arduino IDE.

--- Display/src/main.cpp
+++ Display/src/main.cpp
@@ -1,18 +1,68 @@
-#include <Arduino.h>
+/*
+  Simple Display fireware for ILI9341 LCD
 
-// put function declarations here:
-int myFunction(int, int);
+*/
+#include <ros.h>
+// #include <std_msgs/Empty.h>
+#include <std_msgs/Float32.h>
+#include "SPI.h"
+#include "Adafruit_GFX.h"
+#include "Adafruit_ILI9341.h"
+#define TFT_DC 9
+#define TFT_CS 10
+bool update = true;
+float voltage;
+float oldVoltage;
+long unsigned timer;
+float dis_voltage;
 
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+Adafruit_ILI9341 tft = Adafruit_ILI9341(TFT_CS, TFT_DC);
+
+ros::NodeHandle nh;
+
+void messageCb(const std_msgs::Float32 & voltage_level){
+  voltage = voltage_level.data;
+}
+ros::Subscriber<std_msgs::Float32> sub_voltage("voltage_level", &messageCb);
+
+ void setup() {
+//-------------------------------------------
+  nh.initNode();
+  nh.subscribe(sub_voltage);
+  tft.begin();
+  tft.fillScreen(ILI9341_WHITE);
+  tft.setTextColor(ILI9341_RED);
+  tft.setCursor(10, 10);
+  tft.setTextSize(4);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  check_for_update();
+  display();
+  nh.spinOnce();
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+void check_for_update(){
+  if(millis()-timer>=500){
+    update = true;
+  }
+}
+
+void display(){
+  if(update){
+    tft.setTextColor(ILI9341_WHITE);
+    display_voltage(oldVoltage);
+    tft.setTextColor(ILI9341_RED);
+    display_voltage(voltage);
+    timer = millis();
+    update = false;
+  }
+}
+
+void display_voltage(float voltageValue){
+  tft.setCursor(70,120);
+  if(voltageValue == voltage){
+    oldVoltage = voltage;
+  }
+  tft.println(voltageValue);
 }
