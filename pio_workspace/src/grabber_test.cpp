c2ba732622cd6892cb4d87c3d1f0918f14a3fef5
anna joy <146257353+thirteenlizards@users.noreply.github.com>
2025-01-21 21:10:32 -0500

    grabber_test.cpp

--- pio_workspace/src/grabber_test.cpp
+++ pio_workspace/src/grabber_test.cpp
@@ -1,12 +1,3 @@
-/* Sweep
- by BARRAGAN <http://barraganstudio.com>
- This example code is in the public domain.
-
- modified 8 Nov 2013
- by Scott Fitzgerald
- https://www.arduino.cc/en/Tutorial/LibraryExamples/Sweep
-*/
-
 #include <Servo.h>
 
 Servo myservo;  // create Servo object to control a servo
@@ -24,6 +15,7 @@ void loop() {
     myservo.write(pos);              // tell servo to go to position in variable 'pos'
     delay(15);                       // waits 15 ms for the servo to reach the position
   }
+ 
   //for (pos = 300; pos >= 0; pos -= 1) { // goes from x degrees to 0 degrees
    // myservo.write(pos);              // tell servo to go to position in variable 'pos'
    // delay(15);                       // waits 15 ms for the servo to reach the position
