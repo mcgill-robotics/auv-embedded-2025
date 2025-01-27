e8795c0e7a59701e937ead5def0a9596e2b8472d
dotpotluck <dorothym2488@gmail.com>
2025-01-27 17:29:56 +0800

    commented adc_sensors.cpp & adc_sensors.h

--- pio_workspace/lib/power_custom/adc_sensors.cpp
+++ pio_workspace/lib/power_custom/adc_sensors.cpp
@@ -1,6 +1,21 @@
+/* 
+Revision 1.0
+Commenter: Dorothy Ma
+Date: Jan 27, 2025
+
+FUNCTIONS TO USE VOLTAGE- AND CURRENT-SENSING WITH ADCS:
+1) call "begin()" to initialize the ADCs and indicate whether to enableVoltage- and or enableCurrent-sensing
+2) call "senseVoltage()" and "senseCurrent()" to retrieve updated measurements
+*/
+
 #include "adc_sensors.h"
 
 ADCSensors::ADCSensors() {
+  /* 
+  CONSTRUCTOR TO INITIALIZE AN "EMPTY" OBJECT:
+  1) voltageEnabled and currentEnabled flags are set to false
+  2) the arrays for holding raw, computed, and meaningful-to-human values of the sensor are initialized to 0
+  */
   voltageEnabled = false;
   currentEnabled = false;
 
@@ -18,6 +33,12 @@ ADCSensors::ADCSensors() {
 }
 
 bool ADCSensors::begin(bool enableVoltage, bool enableCurrent, TwoWire* wire) {
+  /*
+  INTIALIZATION OF THE SENSOR FOR VOLTAGE AND OR CURRENTS SENSING
+  Initializes the I2C buses for sensors using their respective addresses (defined in header)
+  ARGUMENTS: flags for voltage sensing, current sensing, and wire
+  RETURNS: true if the sensors are successfully initialized
+  */
   voltageEnabled = enableVoltage;
   currentEnabled = enableCurrent;
 
@@ -36,6 +57,11 @@ bool ADCSensors::begin(bool enableVoltage, bool enableCurrent, TwoWire* wire) {
 }
 
 float* ADCSensors::senseVoltage() {
+  /*
+  STORES MEANINGFUL-TO-HUMANS VOLTAGE VALUES USING convertVoltage FUNCTION IN voltageValues ARRAY
+  Returns: pointer to voltageValues[0-1] array with the converted voltages
+  Will be -ve if voltageEnabled = false
+  */
   if (voltageEnabled) {
     refreshVoltage();
     voltageValues[0] = convertVoltage(computedADCVoltage[0]);
@@ -48,6 +74,11 @@ float* ADCSensors::senseVoltage() {
 }
 
 float* ADCSensors::senseCurrent() {
+  /*
+  STORES MEANINGFUL-TO-HUMANS CURRENT VALUES USING convertCurrent FUNCTION IN currentValues ARRAY
+  Returns: pointer to currentValues[0-7] array with the converted currents
+  Will be -ve if currentEnabled = false
+  */
   if (currentEnabled) {
     refreshCurrent();
     for (int i = 0; i < 8; i++) {
@@ -62,6 +93,11 @@ float* ADCSensors::senseCurrent() {
 }
 
 void ADCSensors::refreshVoltage() {
+  /*
+    PERFORMS ONE READING OF THE ADC FOR VOLTAGE AND UPDATES THE computedADCVoltage array
+    Read and stores the raw ADC value from channels 0 and 1 to rawADCVoltage[0-1] array
+    Stores the voltage computed by "computeVolts" function to computedADCVoltage array
+  */
   rawADCVoltage[0] = adcVoltage.readADC_SingleEnded(0);
   rawADCVoltage[1] = adcVoltage.readADC_SingleEnded(1);
 
@@ -70,6 +106,12 @@ void ADCSensors::refreshVoltage() {
 }
 
 void ADCSensors::refreshCurrent() {
+  /*
+    PERFORMS ONE READING OF THE ADC FOR CURRENTS 1 and 2 AND UPDATES THE computedADCCurrent array
+    Read and stores the raw ADC value from channels 0 to 3 to rawADCCurrent[0-7] array
+    Stores the current computed by "computeVolts" function to computedADCCurrent array
+    Stores adcCurrent1 values into 0-3 and adcCurrent2 values into 4-7
+  */
   for (int i = 0; i < 4; i++) {
     rawADCCurrent[i] = adcCurrent1.readADC_SingleEnded(i);
     computedADCCurrent[i] = adcCurrent1.computeVolts(rawADCCurrent[i]);
@@ -82,9 +124,21 @@ void ADCSensors::refreshCurrent() {
 }
 
 float ADCSensors::convertVoltage(float adcVoltage) {
+  /* 
+  CONVERT ADC VALUES TO REAL-LIFE VOLTAGE VALUES BASED ON CIRCUIT COMPONENTS
+  1+R41/R42 = 3
+  VBAT LOW: 12.8  | ADC INPUT: 0.18
+  VBAT NOM: 14.8  | ADC INPUT: 1.384
+  VBAT HIGH: 16.8 | ADC INPUT: 2.586
+  */
   return (adcVoltage * 2) * (16.8 - 12.8) / (2.586 - 0.180) + 12.5;
 }
 
 float ADCSensors::convertCurrent(float adcCurrent) {
+  /* 
+  CONVERT ADC VALUES TO REAL-LIFE CURRENT VALUES BASED ON CIRCUIT COMPONENTS
+  Rs * Imax * Gain = Vomax
+  Vomax = 0.005 * 10 * 50 = 2.5
+  */
   return (((adcCurrent * 2 ) / 50 ) / 0.005);
 }
--- pio_workspace/lib/power_custom/adc_sensors.h
+++ pio_workspace/lib/power_custom/adc_sensors.h
@@ -1,3 +1,22 @@
+/* 
+Revision 1.0
+Commenter: Dorothy Ma
+Date: Jan 27, 2025
+
+HEADER FILE FOR ADC_SENSOR FOR CURRENT & VOLTAGE SENSING
+* adcCurrent1, adcCurrent2, adcVoltage: Adafruit_ADS1115 sensor drivers
+METHODS:
+* senseVoltage, senseCurrent: returns an array pointer to current measurements
+* refreshVoltage, refreshCurrent: reads the ADC values and uses computeVolts and stores into arrays
+* convertVoltage, convertCurrent: converts computed values to real-world values and stores into arrays
+
+VARIABLES:
+* voltageEnabled, currentEnabled: flags to indicate which component to measure
+* rawADCVoltage, rawADCCurrent: arrays for raw measurements from the ADC
+* computedADCVoltage, computedADCCurrent: arrays for measurements from ADC converted by the ADS1X15 computeVolts method
+* voltageValues, currentValues: arrays for V and I values calculated using real-world components with meaningful values
+*/
+
 #ifndef ADC_SENSORS_H
 #define ADC_SENSORS_H
 
