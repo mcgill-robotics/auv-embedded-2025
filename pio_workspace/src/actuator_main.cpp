b8c42edade55ad393f38847a58086814f2d54139
Andrew <andrew.m.kan@gmail.com>
2025-04-05 13:18:13 -0400

    add actuator ros base code

--- pio_workspace/src/actuator_main.cpp
+++ pio_workspace/src/actuator_main.cpp
@@ -1,13 +1,30 @@
 #ifdef ACTUATOR_H
 
 #include "actuator_main.h"
+#include <Servo.h>
+#include <ros.h>
+#include <std_msgs/UInt16.h>
 
-void actuator_setup(){
-    // put your setup code here, to run once:
+ros::NodeHandle nh;
+
+Servo myServo;
+
+void servoCallback(const std_msgs::UInt16& msg) {
+  int pos = constrain(msg.data, 0, 180);  // Limit angle between 0 and 180
+  myServo.write(pos);
 }
 
-void actuator_loop(){
-    // put your main code here, to run repeatedly:
+ros::Subscriber<std_msgs::UInt16> sub("servo_position", &servoCallback);
+
+void actuator_setup() {
+  myServo.attach(9);  // Connect servo signal wire to pin 9
+  nh.initNode();
+  nh.subscribe(sub);
+}
+
+void actuator_loop() {
+  nh.spinOnce();
+  delay(10);
 }
 
 #endif
