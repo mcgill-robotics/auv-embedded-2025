1ff29a495e6d14adb6427ed7929cf94a676996d1
anna joy <146257353+thirteenlizards@users.noreply.github.com>
2025-04-04 21:59:13 -0400

    Update grabber_test.cpp
    new version with blinky (thx Jay)

--- pio_workspace/test/actuator-tests/grabber_test.cpp
+++ pio_workspace/test/actuator-tests/grabber_test.cpp
@@ -1,23 +1,30 @@
+// CONNECT TO PORT J4
+
+
 #include <Servo.h>
-
-Servo myservo;  // create Servo object to control a servo
-// twelve Servo objects can be created on most boards
-
-int pos = 0;    // variable to store the servo position
-
-void setup() {
-  myservo.attach(8);  // attaches the servo on pin 3 to the Servo object
-}
-
-void loop() {
-  for (pos = 0; pos <= 100; pos += 1) { // goes from 0 degrees to x degrees
-    // in steps of 1 degree
-    myservo.write(pos);              // tell servo to go to position in variable 'pos'
-    delay(15);                       // waits 15 ms for the servo to reach the position
-  }
  
-  //for (pos = 300; pos >= 0; pos -= 1) { // goes from x degrees to 0 degrees
-   // myservo.write(pos);              // tell servo to go to position in variable 'pos'
-   // delay(15);                       // waits 15 ms for the servo to reach the position
- // }
+Servo myservo;  // create servo object to control a servo
+                // twelve servo objects can be created on most boards
+ 
+int pos = 0;    // variable to store the servo position
+ 
+void setup()
+{
+  pinMode(13,OUTPUT);
+  digitalWrite(13,HIGH);
+  myservo.attach(9);  // attaches the servo on pin 9 to the servo object
+}
+ 
+void loop()
+{
+  for(pos = 0; pos <= 180; pos += 1) // goes from 0 degrees to 180 degrees
+  {                                  // in steps of 1 degree
+    myservo.write(pos);              // tell servo to go to position in variable 'pos'
+    delay(15);                       // waits 15ms for the servo to reach the position
+  }
+  for(pos = 180; pos>=0; pos-=1)     // goes from 180 degrees to 0 degrees
+  {                                
+    myservo.write(pos);              // tell servo to go to position in variable 'pos'
+    delay(15);                       // waits 15ms for the servo to reach the position
+  }
 }
