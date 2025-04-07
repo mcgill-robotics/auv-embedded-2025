35c9b5f97b09637eb62b7f58b5436c8657b9bb9c
Andrew <andrew.m.kan@gmail.com>
2025-04-07 18:05:37 -0400

    Add actuator sweep subscriber

--- pio_workspace/src/actuator_main.cpp
+++ pio_workspace/src/actuator_main.cpp
@@ -4,27 +4,45 @@
 #include <Servo.h>
 #include <ros.h>
 #include <std_msgs/UInt16.h>
+#include <std_msgs/Bool.h>
 
 ros::NodeHandle nh;
 
 Servo myServo;
 
-void servoCallback(const std_msgs::UInt16& msg) {
-  int pos = constrain(msg.data, 0, 180);  // Limit angle between 0 and 180
-  myServo.write(pos);
+void servoPosition_CB(const std_msgs::UInt16& msg) {
+  myServo.write(msg.data);
 }
 
-ros::Subscriber<std_msgs::UInt16> sub("servo_position", &servoCallback);
+void servoSweep_CB(const std_msgs::Bool& msg) {
+  if (msg.data) {
+    for (int pos = 0; pos <= 180; pos += 1) {                                 
+      myServo.write(pos);
+      delay(15);
+    } 
+  }
+  else {
+    for (int pos = 180; pos >= 0; pos -= 1) {                                
+      myServo.write(pos);
+      delay(15);
+    } 
+}
+}
+
+ros::Subscriber<std_msgs::UInt16> sub("servo/position", &servoPosition_CB);
+ros::Subscriber<std_msgs::Bool> sub2("servo/sweep", &servoSweep_CB);
 
 void actuator_setup() {
-  myServo.attach(9);  // Connect servo signal wire to pin 9
+  pinMode(9, OUTPUT);
   nh.initNode();
   nh.subscribe(sub);
+  nh.subscribe(sub2);
+  myServo.attach(9);
 }
 
 void actuator_loop() {
   nh.spinOnce();
-  delay(10);
+  delay(1);
 }
 
 #endif
